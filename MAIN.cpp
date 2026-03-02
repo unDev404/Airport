c0b80c6bb5e77cc5dfd3ab254f14fa955a60fb8b
unDev404 <lchristopher2801@gmail.com>
2026-03-01 21:26:53 -0400

    Add files via upload

--- MAIN.cpp
+++ MAIN.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
+struct aerolinea //ciudades con aeropuerto, para los destinos 
+{
+    string nombre;
+    string codigo;
+};
+
+
+
+
 struct vuelos
 {
-    string ID; //código único de identificación Ej: AN023 (de momento en el struct)
-    string aerolinea; //nombre de la empresa a la que pertenece el avión
-    string origen; //ciudad de la que viene el vuelo
+    struct aerolinea; //nombre de la empresa a la que pertenece el avión ARRAY STRING
+    string ID; //código único de identificación Ej: AN023 (CLAVE PARA HASH)
+    string origen; //ciudad de la que viene el vuelo ARRAY STRING
     string destino; //ciudad a la que se dirige el vuelo
     string prioridad; //preferencia jerarquica del vuelo {Comercial, VIP, Emergencia, Reprogramado, Cancelado}
     int horaProgramada; //Hora en la que debería estar en la pista del aeropuerto
@@ -15,40 +24,42 @@ struct vuelos
     int día[7]; //Día en que el avión está en la pista, cola circular (de momento)
 };
 
-//FUNCIONES PRINCIPALES
-
-/*
-1. Registro de Vuelos
-2. Clasificación por Prioridad/horario
-3. Filtros de búsqueda
-4. Observar Lista de Vuelos
-
-*/
 
 
-//REGISTRO DE VUELOS
+void mostrarArray(string opciones[], int tam)
+{
+for(int i = 0; i < tam; i++)
+{
+    cout<< i+1 << ". " << opciones[i]<< endl;
+}
+}
 
-/*
-1. crear vuelos en una lista
-2. Aquí se usa la tabla hash, el ID es la clave y el struct la info.
-3. Ver Lista de Vuelos
-*/
+int selecArray(int tam)
+{
+    int x = 0;
+    bool verify = true;
+    do
+    {
+        cout << "\nIngresa tu eleccion: ";
+        cin >> x;
+        if(x>0 && x<tam) verify = false;
+        else cout << "\nOpcion Invalida, Intente nuevamente" << endl;
 
+    }while(verify);
+   
+    return x-1;
+}
 
-//CLASIFICACIÓN POR PRIORIDAD/HORARIO
+int main()
+{
 
-/*
-1. Aquí se usa el arbol Binario, recibe la lista de vuelo y los ordena por Prioridad/horario
-*/
+    string aerolíneas[5] = {"Conviasa", "Rutaca", "Avior", "LASER", "KAYAK"}; //aerolíneas disponibles en el aeropuerto
+    string ciudades[4] = {"Puerto Ordaz", "Maturin", "Barquisimeto", "Caracas"}; //aerolíneas disponibles en el aeropuerto
 
+    mostrarArray(ciudades, 4);
+    int selection = selecArray(4);
 
-//ADMINISTRACIÓN DE ATERRIZAJES/DESPEGUES
-
-/*
-1. Recibe los vuelos ordenados
-2. separa los vuelos por despegue/aterrizaje en colas
-3. Simular que se tiene 3 pistas en simultáneo
-4. Desarrollo de la operación de Pista hasta Finalizado
-5. usar una función random para que exista la posibilidad de que sean cancelados o reprogramados
-    de ser cancelados, se eliminan; de ser reprogramados, vuelven al árbol quizás con menor prioridad
-*/
+    cout << selection+1 << ". " << ciudades[selection] << endl;
+    
+    return 0;
+}
