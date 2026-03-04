e2e944ba02cb7bd98c60f5f17faeb5f2caba938a
Doge-485 <alainflores1991@gmail.com>
2026-03-03 20:02:05 -0400

    Jerarquizacion por calendario

--- arbol.h
+++ arbol.h
@@ -11,8 +11,21 @@ struct nodoArBin
 };
 
 bool mayorPrioridad(vuelos a, vuelos b);
+{
+// COMPARAR POR DÍA
+    if (a.dia < b.dia) return true; // Si el día de 'a' es antes, tiene prioridad
+    if (a.dia > b.dia) return false; // Si el día de 'a' es después, no la tiene
+// COMPARAR POR HORA SI LOS DÍAS SON IGUALES
+    [cite_start]if (a.horaProgramada < b.horaProgramada) return true; [cite: 10]
+    [cite_start]if (a.horaProgramada == b.horaProgramada) { [cite: 11]
+        return a.prioridad < b.prioridad; [cite_start]// EMERGENCIA (0) es mayor que VIP (1) [cite: 11, 12]
+    }
+    [cite_start]return false; [cite: 13]
+}
 
+        
 void insertarEnArBin(nodoArBin*& raiz, vuelos v);
 
 vuelos extraerMasPrioritario(nodoArBin*& raiz);
+
 #endif
